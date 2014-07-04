026631fd6a04997b32f24fdbe167576597655df0
fernando-fte <fernando.fte@gmail.com>
2014-07-04 12:22:25 -0300

    App:sql>selector: Cria aplicação para tratamento e comunicação com o servidor
    Esta aplicação deve tratar os tipos de comunicações "SELECT", "INSERT",
    "UPDATE".

---
+++ assets/library/php/sql.selector.php
@@ -0,0 +1,8 @@
+<?php
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Aplicação de comunicação com o servidor dos tipos query   #
+# sendo estes quando for "SELECT", "INSERT", UPDATE         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+?>
