d138d0d18647fb21a4fcd2ea17aa4f3fe6951a13
fernando-fte <fernando.fte@gmail.com>
2014-07-04 12:28:19 -0300

    App:sql>seletor: Cria função "query($sql)"
    Esta função vai tratar os parametros e enviar a solicitação ao banco de
    dados de a cordo com o parametro dentro da string "$sql".

--- assets/library/php/sql.selector.php
+++ assets/library/php/sql.selector.php
@@ -5,4 +5,33 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# função de interação com query para o 'sql'
+#
+function query($sql){
+    # # # # #
+    # # Descreve valores recebidos de "query($sql){}"
+    # $sql = recebe uma string com os valores compilados
+    # # # # #
+
+    # adiciona em "$con" a classe responsável pela conexão
+    $con = new _conecta;
+
+    # abre conexao
+    $con->AbreConexao();
+
+    # seta o valor query
+    $Sel = mysql_query($sql) or die(mysql_error());
+
+    # fecha a conexao
+    $con->FechaConexao();
+
+    # retorna query
+    return $Sel;
+}
+#
+# Fim de  "função de interação com query para o 'sql'"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 ?>
