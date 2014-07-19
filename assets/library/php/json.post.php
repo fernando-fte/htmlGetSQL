0cc103f4b1e384409353e713e2515898394f27ac
fernando-fte <fernando.fte@gmail.com>
2014-07-19 11:55:03 -0300

    App:json.post: inclui funções de tratamento e manipulação para conexão do banco de dados

--- assets/library/php/json.post.php
+++ assets/library/php/json.post.php
@@ -11,6 +11,10 @@
 # inclui classe de conexão ao banco de dados
 include 'sql.connect.php';
 
+# inclui funções de tratamento e manipulação para conexão do banco de dados
+include 'sql.selector.php';
+
+
 #
 # Fim de "Inclui funções e regras pre-determinadas"
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
