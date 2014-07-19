d9d19e28655393e8e389e4a91e2d8fbd3c48822c
fernando-fte <fernando.fte@gmail.com>
2014-07-19 11:50:17 -0300

    App: Cria "json.post" para receber e tratar os arquivos do json
    Esta aplicação deve receber como post do $ajax vindo de
    "submit.php.coffee", ela é responsavel pela transição e recebimento dos
    dados do servidor, tratando as solicitações e as respostas, devolvendo
    as para o json.

---
+++ assets/library/php/json.post.php
@@ -0,0 +1,5 @@
+<?php
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# APLICAÇÃO DE TRATAMENTO E TRAMITAÇÃO DE DADOS JSON        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+?>
