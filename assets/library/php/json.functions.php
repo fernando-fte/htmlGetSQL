036d0a4d2e86e7a191fe64400d8ad2d79f6bfa35
fernando-fte <fernando.fte@gmail.com>
2014-07-19 13:24:20 -0300

    App:json.post>funtions: quando não existir a array "regra", adiciona as configurações necessárias

--- assets/library/php/json.functions.php
+++ assets/library/php/json.functions.php
@@ -7,6 +7,14 @@
 # função para validar se as entradas do $_POST são corretas
 function f_json_post($post) {
 
+    # #
+    # quando não existir a array "regra", adiciona as configurações necessárias
+    if (!array_key_exists("regra", $post)) {
+
+
+    }
+    # Fim da 'quando não existir a array "regra", adiciona as configurações necessárias'
+    # #
 
 
 }
