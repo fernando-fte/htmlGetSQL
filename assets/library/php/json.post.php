41b92f4e1393e0888556bba9c3a40545ce6400ba
fernando-fte <fernando.fte@gmail.com>
2014-07-19 11:54:21 -0300

    App:json.post: Inclui a conexao com o banco

--- assets/library/php/json.post.php
+++ assets/library/php/json.post.php
@@ -8,6 +8,9 @@
 # Inclui funções e regras pre-determinadas
 #
 
+# inclui classe de conexão ao banco de dados
+include 'sql.connect.php';
+
 #
 # Fim de "Inclui funções e regras pre-determinadas"
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
