41852680c4ceba87fec6e9c874538d5a2cbd3d6e
fernando-fte <fernando.fte@gmail.com>
2014-07-19 13:23:54 -0300

    App:json.post>funtions: função para validar se as entradas do $_POST são corretas

--- assets/library/php/json.functions.php
+++ assets/library/php/json.functions.php
@@ -3,4 +3,14 @@
 # FUNÇÕES DE APOIO PARA O TRATAMENTO DE JSON        		#
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# # # # #
+# função para validar se as entradas do $_POST são corretas
+function f_json_post($post) {
+
+
+
+}
+# função para validar se as entradas do $_POST são corretas
+# # # # #
+
 ?>
