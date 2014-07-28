d26363aedeec4e2418190ee14b219d5add8ac46f
fernando-fte <fernando.fte@gmail.com>
2014-07-28 19:33:28 -0300

    App:json: Sobe arquivos completos

--- assets/library/php/json.functions.php
+++ assets/library/php/json.functions.php
@@ -1,7 +1,250 @@
 <?php  
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# FUNÇÕES DE APOIO PARA O TRATAMENTO DE JSON        		#
+# FUNÇÕES DE APOIO PARA O TRATAMENTO DE JSON                #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# # # # #
+# função para transformar [object Object] json em array
+function f_object_to_array($object) {
+
+    # verifica se $object não é um [object Object] e nem uma array
+    if(!is_object($object) && !is_array($object)) { 
+
+        # devolve valor a função sem modificações
+        return $object; 
+    }
+
+    # valida se a $object é um [object Object]
+    if(is_object($object)) {
+
+        # adiciono em $object ele mesmo processado com a função get_object_vars()
+        $object = get_object_vars( $object);
+    }
+   
+   # retorna uma array pameada de $object com 'objectToArray'
+   return array_map( 'objectToArray', $object );
+}
+# função para transformar [object Object] json em array
+# # # # #
+
+
+# # # # #
+# função para montar os parametros para um select
+function f_json_where($post) {
+
+    # verifica se o valor recebido em $post possui a array "select"
+    if (array_key_exists("select", $post)) {
+
+        # # # 
+        # separa arrays recebidas de $post
+
+        # adiciona em $temp>key uma lista com os valores das chaves de $post>select
+        $temp['key'] = array_keys($post['select']);
+
+        # adiciona em $temp>val uma lista com os valores de cada array em $post>select
+        $temp['val'] = array_values($post['select']);
+
+        # adiciona em $temp>key>count quantas arrays foram adicionadas em $temp>key
+        $temp['key']['count'] = count($temp['key']);
+
+        # adiciona em $temp>val>count quantas arrays foram adicionadas em $temp>val
+        $temp['val']['count'] = count($temp['val']);
+
+        # separa arrays recebidas de $post
+        # # # 
+
+
+        # # # 
+        # trata valores compilados, validando e montando regra na seção WHERE conforme o indicador
+
+        # selecina em loop cada array de $temp>val
+        for ($temp['count']=0; $temp['count'] < $temp['val']['count']; $temp['count']++) {
+
+            # #
+            # quando $post>regra>where tiver o tipo de seleção WHERE especifico
+            if ($post['regra']['where'] == "LIKE" or $post['regra']['where'] == "=") {
+
+                # configura a primeira entrada do loop, que pode ser a unica, selecionando apenas uma coluna
+                if($temp['count'] <= 0) {
+
+                    # monta em $temp>where a chamda da regra de validação de select()
+                    $temp['where'] = '`'.$temp['key'][$temp['count']].'` '.$post['regra']['where'].' \''.$temp['val'][$temp['count']].'\' ';
+                }
+
+                # configura a partir da segunda entrada do loop, para selecionar mais de uma coluna
+                else{
+
+                    # adiciona em $temp>where ele mesmo mais a concatenação "AND" e a segunda leva da regra de seleção
+                    $temp['where'] = $temp['where'] .'AND `'.$temp['key'][$temp['count']].'` '.$post['regra']['where'].' \''.$temp['val'][$temp['count']].'\' ';
+                }
+            }
+
+            # #
+            # quando $post>regra>where tiver o tipo de seleção WHERE relativo
+            else if ($post['regra']['where'] == "LIKE%") {
+
+                # configura a primeira entrada do loop, que pode ser a unica, selecionando apenas uma coluna
+                if($temp['count'] <= 0) {
+
+                    # monta em $temp>where a chamda da regra de validação de select()
+                    $temp['where'] = '`'.$temp['key'][$temp['count']].'` LIKE \'%'.$temp['val'][$temp['count']].'%\' ';
+                }
+
+                # configura a partir da segunda entrada do loop, para selecionar mais de uma coluna
+                else{
+
+                    # adiciona em $temp>where ele mesmo mais a concatenação "AND" e a segunda leva da regra de seleção
+                    $temp['where'] = $temp['where'] .'AND `'.$temp['key'][$temp['count']].'` LIKE \'%'.$temp['val'][$temp['count']].'%\' ';
+                }
+            }
+        }
+
+        # trata valores compilados, validando e montando regra na seção WHERE conforme o indicador
+        # # # 
+
+
+        # # # 
+        # finaliza montagem da das regras dentro das arrays de retorno e adiciona em $regra, lembrando que a função leva em conta a array enviada
+
+        # acrecenta em $regra>'WHERE ' os valores manipulados e tratados acrecentados em $temp>where
+        $regra['WHERE '] =  $temp['where'];
+
+        # acrecenta em $regra>'WORDER BY ' os dados de ordenação de resultados, recebidos em $post>regra>order
+        $regra['ORDER BY '] =  '`'.$post['regra']['order']['to'].'` '.$post['regra']['order']['by'].'';
+
+        # acrecenta em $regra>'LIMIT ' a quantidade de dados a serem retornados, recebido em $post>regra>limit
+        $regra['LIMIT '] =  $post['regra']['limit'];
+
+        # apaga $temp na posição atua
+        unset($temp);
+
+        # retorna regra para a função
+        return $regra;
+        
+        # finaliza montagem da das regras dentro das arrays de retorno e adiciona em $regra, lembrando que a função leva em conta a array enviada
+        # # # 
+    }
+}
+# função para montar os parametros para um select
+# # # # #
+
+
+# # # # #
+# função para retornar apenas o campo "values" recebido do banco de dados
+function f_json_values($select){
+
+    # # # 
+    # caso exista a array "values" na raiz de $select, sendo apenas um retorno do servidor
+    if (array_key_exists("values", $select)) {
+
+        # acrecenta na string $return os valores de $select>values
+        $return = $select['values'];
+    }
+
+    # # # 
+    # caso não exista a array "values" na raiz de $select, podendo ser mais de um valor retornado
+    else{
+
+        # conta quantas ocorrencias
+        $temp['count'] = count($select);
+
+        # excecuta loop para tratar cada resposta do servidor
+        for ($temp['position'] = 0; $temp['position'] < $temp['count']; $temp['position']++) {
+
+            # quando for o primeiro o primeiro resultado
+            if($temp['position'] <= 0) {
+
+                # adiciona em $temp>return os "values" da posição em $select>%%>values
+               $temp['return'] = $select[$temp['position']]['values'];
+            }
+
+            # quando do segundo resultado em diante
+            else{
+
+                # acrecenta em $temp>return ele mesmo mais os "values" da posição em $select>%%>values
+                $temp['return'] = $temp['return'] .", ". $select[$temp['position']]['values'];
+            }
+
+            # acrecenta em $return os valores tratados em $temp>return
+            $return = $temp['return'];
+        }
+    }
+
+    # retorna os valores para a função
+    return $return;
+}
+# função para retornar apenas o campo "values" recebido do banco de dados
+# # # # #
+
+
+# # # # #
+# função para validar o status do values>[object Object]
+function f_valida_status ($return) {
+
+    # acrecenta em $return ele mesmo como array
+    $return = (json_decode($return, true));
+
+    # valida se na raiz existe "status"
+    if(array_key_exists('status', $return['0'])){
+
+        # acrecenta m $return>0>status ele mesmo com a font em smallcase
+        $return['0']['status'] = strtolower($return['0']['status']);
+
+        # inicia sequencia de selecao de status
+        switch ($return['0']['status']) {
+
+            # caso seja ativo retorna 'true'
+            case 'active':
+                return true;
+                break;
+            case 'actived':
+                return true;
+                break;
+            case 'ativo':
+                return true;
+                break;
+            case 'desativo':
+                return true;
+                break;
+
+            # caso seja desativado retorna 'false'
+            case 'dasebled':
+                return false;
+                break;
+            case 'desable':
+                return false;
+                break;
+            case 'inative':
+                return false;
+                break;
+            case 'inativo':
+                return false;
+                break;
+            case 'desativo':
+                return false;
+                break;
+
+            # caso seja exluido retorna 'excluded'
+            case 'exclude':
+                return 'excluded';
+                break;
+            case 'removed':
+                return 'excluded';
+                break;
+            case 'excluido':
+                return 'excluded';
+                break;
+        }
+    }
+
+    # quando não houver o atributo status nao retorna 'true'
+    else{
+        return true;
+    }
+}
+# função para validar o status do values>[object Object]
+# # # # #
+
 
 # # # # #
 # função para validar se as entradas do $_POST são corretas
--- assets/library/php/json.post.php
+++ assets/library/php/json.post.php
@@ -22,4 +22,271 @@ include 'json.functions.php'
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Inicia aplicação e configurações
+
+# define em $post os valores de $_POST processado na função f_json_post($_POST);
+$post = f_json_post($_POST);
+
+# verifica se existe o a array "type" em post
+if (array_key_exists('type', $post)) {
+
+    # laço para aplicar ações conforme a seleção
+    switch ($post['type']) {
+
+        # # # # #
+        # quando o $post>type for update, que envia alterações para o servidor
+        case 'update':
+
+            # # #
+            # # 1ª ETAPA: seleciona o banco de dados e substitui os campos alterados guardados em $_POST
+
+            # #
+            # define e seleciona valores do banco de dados
+            
+            # adiciona em $temp>regra os valores de $post na função 'f_json_where'
+            $temp['regra'] = f_json_where($post); 
+            
+            # adiciona em $temp>tabela o valor referente a tabela com tratamento especifico
+            $temp['tabela'] = '`'.$post['table'].'`'; 
+
+            # adiciona em $temp>campos os campos a serem selecionado, no caso "*" todos por padrão
+            $temp['campos'] = array('' => '*');
+
+            # seleciona os valores do banco de dados recebidos da função select() com os dados tratados anteriormente
+            $temp['select'] = select($temp['tabela'], $temp['campos'], $temp['regra']);
+
+            # Fim de "define e seleciona valores do banco de dados"
+            # #
+
+            # mapeia as respostas do servidor como object json para array
+            $temp['return'] = (json_decode($temp['select']['values'], true));
+
+            # mescla o resultado do banco de dados e o valor recebido por post, atualizando os dados e garantindo que nem uma array será desfeita
+            $temp['values'] = array_merge($temp['return'], $post['values']);
+
+            # mapeia os dados de resposta do servidor e transforma em object json
+            $temp['dados']['values'] = json_encode($temp['values']);
+
+
+            # # #
+            # # 2ª ETAPA: Envia arquivos atualizados para o servidor
+
+            # #
+            # define e reordena os valores do banco de dados
+
+            # zera $temp>campo
+            unset($temp['campos']);
+
+            # acrecenta em $temp>campos>index os valores da coluna "index"
+            $temp['campos']['index'] = $post['select']['index'];
+
+            # acrecenta em $temp>campos>segmento os valores da coluna "segmento"
+            $temp['campos']['segmento'] = $post['select']['segmento'];
+
+            # acrecenta em $temp>campos>grupo os valores da coluna "grupo"
+            $temp['campos']['grupo'] = $post['select']['grupo'];
+
+            # acrecenta em $temp>campos>type os valores da coluna "type"
+            $temp['campos']['type'] = $post['select']['type'];
+
+            # acrecenta em $temp>campos>values os dados compilados e configura os caracteres especiais
+            $temp['campos']['values'] = addslashes($temp['select']['values']);
+
+            # retorna os valores organizados em $temp>campos ao $post>select
+            $post['select'] = $temp['campos'];
+
+            # define e reordena os valores do banco de dados
+            # #
+
+            # define que regra de seleção where é "="
+            $post['regra']['where'] = "=";
+
+            # adiciona em $temp>regra os valores processados na função "f_json_where($post"
+            $temp['regra'] = f_json_where($post);
+
+            # envia para a função update() os valores tratados, e atualiza o banco
+            update($temp['tabela'], $temp['dados'], $temp['regra']);
+
+            # acrecenta na string $return a resposta que foi feito o update
+            $return = '[{"update":"true"}]';
+
+            # retorna $return ao json
+            echo $return;
+
+            break;
+        # Fim de "quando o $post>type for update, que envia alterações para o servidor"        
+        # # # # #
+
+        # # # # #
+        # quando o $post>type for insert, que insere um novo campo na tabela
+        case 'insert':
+
+            # # #
+            # # 1ª ETAPA: verifica a existencia de um index e estabelece uma nova etrada de acordo com o resultado, garantindo que o index será maior que o ultimo valor já existente 
+
+            # #
+            # define e seleciona valores do banco de dados
+
+            # adiciona em $post>regra>order>by o valor "DESC", para selecionar apartir do ultimo registro
+            $post['regra']['order']['by'] = "DESC";
+            
+            # adiciona em $temp>regra os valores de $post na função 'f_json_where'
+            $temp['regra'] = f_json_where($post); 
+            
+            # adiciona em $temp>tabela o valor referente a tabela com tratamento especifico
+            $temp['tabela'] = '`'.$post['table'].'`'; 
+
+            # adiciona em $temp>campos os campos a serem selecionado, no caso "*" todos por padrão
+            $temp['campos'] = array('' => '*');
+
+            # seleciona os valores do banco de dados recebidos da função select() com os dados tratados anteriormente
+            $temp['select'] = select($temp['tabela'], $temp['campos'], $temp['regra']);
+
+            # Fim de "define e seleciona valores do banco de dados"
+            # #
+
+            # #
+            # reconhece e trata o valor do index
+
+            # caso $temp>select seja vazio
+            if ($temp['select'] == 'Empty') { 
+
+                # adiciona $post>select>index com 1
+                $post['select']['index'] = 1; 
+            }
+
+            # caso $temp>select tenha recebudo algum valor, verifica se existe array index
+            else if (array_key_exists('index', $temp['select'])) {
+
+                # adiciona em $post>select>index +1, para garantir que o ele será maior que o que contem no banco de dados
+                $post['select']['index'] = $temp['select']['index']+'1';
+            }
+
+            # reconhece e trata o valor do index
+            # #
+
+
+            # # #
+            # # 2ª ETAPA: Estabelece valores padões para o values do banco de dados e organiza as entradas
+
+            # # 
+            # adiciona em $post>select>values as configurações padrões
+            $post['select']['values'] = '{"status":"Inativo", "template-toogle":{"status":"Ativo"}}';
+
+            # adiciona em $post>select>values as configurações padrões
+            # #
+
+            # #
+            # define e reordena os valores do banco de dados
+
+            # zera $temp>campo
+            unset($temp['campos']);
+
+            # acrecenta em $temp>campos>index os valores da coluna "index"
+            $temp['campos']['index'] = $post['select']['index'];
+
+            # acrecenta em $temp>campos>segmento os valores da coluna "segmento"
+            $temp['campos']['segmento'] = $post['select']['segmento'];
+
+            # acrecenta em $temp>campos>grupo os valores da coluna "grupo"
+            $temp['campos']['grupo'] = $post['select']['grupo'];
+
+            # acrecenta em $temp>campos>type os valores da coluna "type"
+            $temp['campos']['type'] = $post['select']['type'];
+
+            # acrecenta em $temp>campos>values os dados compilados e configura os caracteres especiais
+            $temp['campos']['values'] = addslashes($temp['select']['values']);
+
+            # define e reordena os valores do banco de dados
+            # #
+
+
+            # # #
+            # # 3ª ETAPA: Envia os dados para o servidor, e envia resposta para o json
+
+            # incherta os valores tratados no banco de dados pela função insert()
+            insert($temp['tabela'], $temp['campos']);
+
+            # acrecenta em $return a resposta tratada para o json
+            $return = '[{"index":"'.$temp['campos']['index'].'", "status":"desabled", "type":"update"}]';
+
+            # retorna ao json
+            echo $return;
+
+            # zero temp;
+            unset($temp);
+            break;
+        # Fim de "quando o $post>type for insert, que insere um novo campo na tabela"
+        # # # # #
+
+        # # # # #
+        # quando o $post>type for select, seleciona os valores do banco de dados conforme a solicitação
+        case 'select':
+
+            # # #
+            # # 1ª ETAPA: Seleciona o banco de dados, e acrecenta resposta a $return com um object
+
+            # #
+            # define e seleciona valores do banco de dados
+            
+            # adiciona em $temp>regra os valores de $post na função 'f_json_where'
+            $temp['regra'] = f_json_where($post); 
+            
+            # adiciona em $temp>tabela o valor referente a tabela com tratamento especifico
+            $temp['tabela'] = '`'.$post['table'].'`'; 
+
+            # adiciona em $temp>campos os campos a serem selecionado, no caso "*" todos por padrão
+            $temp['campos'] = array('' => '*');
+
+            # seleciona os valores do banco de dados recebidos da função select() com os dados tratados anteriormente
+            $temp['select'] = select($temp['tabela'], $temp['campos'], $temp['regra']);
+
+            # adiciona em $return um object Object tratado com a função f_json_values() com resposta do servidor em $temp>select
+            $return = '['.f_json_values($temp['select']).']';
+
+            # Fim de "define e seleciona valores do banco de dados"
+            # #
+
+
+            # # #
+            # # 2ª ETAPA: Valida se algum dos resultados estão ativos
+ 
+            # verifica se o status é válido
+            if(f_valida_status($return) == true){
+
+                # retorna ao json os valores selecionados
+                echo $return;
+            }
+
+            # verifica se há uma solicitação de arquivos nao ativos
+            if($post['status']){
+
+                # verifico o tipo de solicitação, e retorno ao jquery
+                switch ($post['status']) {
+
+                    # quando $post>status for "update", exibe todos os dados menos os excluidos
+                    case 'update': # quando for upate
+
+                        # filtra todos menos os que forão excluidos  indiretamente
+                        if(f_valida_status($return) != 'excluded'){ echo $return; }
+                        break;
+
+                    # quando $post>status for "recover", exibe apenas os dados marcados como excluidos
+                    case 'recover': 
+
+                        # filtra dados marcados como excluidos
+                        if(f_valida_status($return) == 'excluded'){ echo $return; }
+                        break;
+                }
+            } 
+
+            break;
+        # Fim de "quando o $post>type for select, seleciona os valores do banco de dados conforme a solicitação"
+        # # # # #
+    }
+}
+
+# Fim de inicia aplicação e configurações
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 ?>
