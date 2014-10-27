a4463a71fbc4a5efc17de254c07d45848728936a
fernando-fte <fernando.fte@gmail.com>
2014-10-27 15:44:45 -0200

    App:json.post>update: Remove versão antiga em comentário

--- assets/library/php/json.post.php
+++ assets/library/php/json.post.php
@@ -48,7 +48,7 @@ if (array_key_exists('type', $post)) {
         case 'update':
 
             # # # # #
-            # # 1º CASO: Envia os arquivos para history
+            # # 1ª ETAPA: Envia os arquivos para history, e cria nova seção
 
             # verifica se o valor recebido em $post possui a array "update"
             if (!array_key_exists('update', $post)) {
@@ -58,9 +58,11 @@ if (array_key_exists('type', $post)) {
 
                 # retorna ao JS
                 echo '['.json_encode($temp['return'], true).']';
+            }            
 
-            }
-            
+
+            # # # # #
+            # # 2ª ETAPA: Trata os valores recebidos em update, parra history
 
             # verifica se o valor recebido em $post possui a array "update"
             if (array_key_exists('update', $post)) {
@@ -267,7 +269,6 @@ if (array_key_exists('type', $post)) {
 
                         echo '['.json_encode($temp['return'], true).']';
                     }
-
                 }
 
                 if (!array_key_exists('history', $post['update'])) {
@@ -279,103 +280,6 @@ if (array_key_exists('type', $post)) {
                     echo '['.json_encode($temp['return'], true).']';
                 }
             }
-            // # verifica se o valor recebido em $post possui a array "update"
-            // if (array_key_exists('update', $post)) {
-
-            //    // $post['update'] = 'b1c2d3e4f5';
-            //    // $post['update']['']
-
-            //     # valida se $post>update não possui history
-            //     if (array_key_exists('history', $post['update'])) {
-
-
-
-            //     }
-
-            //     if (array_key_exists('chenge', $post['update'])) {
-
-
-            //     }
-            // }
-
-
-            // print_r($post);
-
-            # # #
-            # # 1ª ETAPA: seleciona o banco de dados e substitui os campos alterados guardados em $_POST
-
-            # #
-            # define e seleciona valores do banco de dados
-
-            // # adiciona em $temp>regra os valores de $post na função 'f_json_where'
-            // $temp['regra'] = f_json_where($post); 
-
-            // # adiciona em $temp>tabela o valor referente a tabela com tratamento especifico
-            // $temp['tabela'] = '`'.$post['table'].'`'; 
-
-            // # adiciona em $temp>campos os campos a serem selecionado, no caso "*" todos por padrão
-            // $temp['campos'] = array('' => '*');
-
-            // # seleciona os valores do banco de dados recebidos da função select() com os dados tratados anteriormente
-            // $temp['select'] = select($temp['tabela'], $temp['campos'], $temp['regra']);
-
-            // # Fim de "define e seleciona valores do banco de dados"
-            // # #
-
-            // # mapeia as respostas do servidor como object json para array
-            // $temp['return'] = (json_decode($temp['select']['values'], true));
-
-            // # mescla o resultado do banco de dados e o valor recebido por post, atualizando os dados e garantindo que nem uma array será desfeita
-            // $temp['values'] = array_merge($temp['return'], $post['values']);
-
-            // # mapeia os dados de resposta do servidor e transforma em object json
-            // $temp['dados']['values'] = json_encode($temp['values']);
-
-
-            // # # #
-            // # # 2ª ETAPA: Envia arquivos atualizados para o servidor
-
-            // # #
-            // # define e reordena os valores do banco de dados
-
-            // # zera $temp>campo
-            // unset($temp['campos']);
-
-            // # acrecenta em $temp>campos>index os valores da coluna "index"
-            // $temp['campos']['index'] = $post['select']['index'];
-
-            // # acrecenta em $temp>campos>segmento os valores da coluna "segmento"
-            // $temp['campos']['segmento'] = $post['select']['segmento'];
-
-            // # acrecenta em $temp>campos>grupo os valores da coluna "grupo"
-            // $temp['campos']['grupo'] = $post['select']['grupo'];
-
-            // # acrecenta em $temp>campos>type os valores da coluna "type"
-            // $temp['campos']['type'] = $post['select']['type'];
-
-            // # acrecenta em $temp>campos>values os dados compilados e configura os caracteres especiais
-            // $temp['campos']['values'] = addslashes($temp['select']['values']);
-
-            // # retorna os valores organizados em $temp>campos ao $post>select
-            // $post['select'] = $temp['campos'];
-
-            // # define e reordena os valores do banco de dados
-            // # #
-
-            // # define que regra de seleção where é "="
-            // $post['regra']['where'] = "=";
-
-            // # adiciona em $temp>regra os valores processados na função "f_json_where($post"
-            // $temp['regra'] = f_json_where($post);
-
-            // # envia para a função update() os valores tratados, e atualiza o banco
-            // // update($temp['tabela'], $temp['dados'], $temp['regra']);
-
-            // # acrecenta na string $return a resposta que foi feito o update
-            // $return = '[{"update":"true", "result":'.$temp['dados']['values'].'}]';
-
-            // # retorna $return ao json
-            // echo $return;
 
             break;
         # Fim de "quando o $post>type for update, que envia alterações para o servidor"        
